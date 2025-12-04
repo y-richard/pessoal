2b25913eaab810574eb2b218991d0e771abbe0d4
y-richard <richard.cesario@escola.pr.gov.br>
2025-12-04 20:43:13 -0300

    dsada

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 function toggleDarkMode() {
   document.body.classList.toggle("dark-mode");
 }
+
+/* ANIMAÇÃO AO ROLAR */
+const elements = document.querySelectorAll(".fade-in, .slide-up");
+
+function checkScroll() {
+  const trigger = window.innerHeight * 0.85;
+
+  elements.forEach(el => {
+    const rect = el.getBoundingClientRect().top;
+    if (rect < trigger) {
+      el.style.animationPlayState = "running";
+    }
+  });
+}
+
+window.addEventListener("scroll", checkScroll);
+checkScroll();
